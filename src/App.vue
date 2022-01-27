0c2ed50ca77f357174e2cbd0d0e546a7e836b645
g.bisurgi <g.bisurgi@herzogkommunikation.de>
2022-01-27 15:34:38 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app">
+        <input v-model="message" type="text">
+        <h2 class="message">{{ message }}</h2>
+        <h3>One More Message</h3>
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        message: 'Hallo Welt',
+      };
+    },
+  };
+</script>
+
+<style>
+    #app {
+        font-size: 18px;
+        font-family: 'Roboto', sans-serif;
+        color: blue;
+    }
+</style>
