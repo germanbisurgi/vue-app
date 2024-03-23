71d1ef0bb5f3e2d34921af735e905c7cf2a742ce
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2024-03-23 22:39:12 +0100

    Updated links

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -4,12 +4,12 @@
       <div class="navigation-items">
         <div class="navigation-item" data-scrollspy-feedback="buttons">
           <router-link to="/">
-            Go to Home
+            Home
           </router-link>
         </div>
         <div class="navigation-item" data-scrollspy-feedback="links">
           <router-link to="/about">
-            Go to About
+            About
           </router-link>
         </div>
       </div>
