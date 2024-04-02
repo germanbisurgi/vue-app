76a15b9f0d2c3d16c887062ffb3ed091de21b62b
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2024-04-02 19:54:13 +0200

    Updated vite config and upgraded packages

--- config/vite.config.js
+++ config/vite.config.js
@@ -1,18 +1,19 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-const path = require('path')
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '../src')
+      '@': resolve(__dirname, '../src')
     }
   },
   server: {
     port: 8080
   },
   preview: {
-    port: 8081
+    port: 8081,
+    strictPort: true
   }
 })
